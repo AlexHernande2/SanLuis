fa5da1f0ec9ffd50df3f896fe2ba909143957755
AlexHernande2 <edalhernandez@uniboyaca.edu.co>
2024-05-03 15:48:23 -0500

    Se agrego la tabla de categorias a la pagina principal

--- view/js/index.js
+++ view/js/index.js
@@ -1,6 +1,6 @@
 
 // Espera a que se cargue completamente el DOM antes de ejecutar el código
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Obtiene una referencia al contenedor del encabezado por su ID
     const headerContainer = document.getElementById('header-container');
     // Especifica la ruta del archivo HTML que contiene el encabezado
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Abre una conexión HTTP GET asíncrona hacia el archivo HTML del encabezado
     xhr.open('GET', headerHtmlFile, true);
     // Define una función para manejar el cambio de estado de la solicitud
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         // Verifica si la solicitud se ha completado y si el código de estado es 200 (OK)
         if (xhr.readyState === 4 && xhr.status === 200) {
             // Si la solicitud es exitosa, actualiza el contenido del contenedor del encabezado
@@ -19,4 +19,21 @@ document.addEventListener('DOMContentLoaded', function() {
     };
     // Envía la solicitud HTTP
     xhr.send();
+
+
+
+    // Carga el pie de página
+    const footerContainer = document.getElementById('footer-container');
+    const footerHtmlFile = 'footer.html';
+    const footerXhr = new XMLHttpRequest();
+    footerXhr.open('GET', footerHtmlFile, true);
+    footerXhr.onreadystatechange = function () {
+        if (footerXhr.readyState === 4 && footerXhr.status === 200) {
+            footerContainer.innerHTML = footerXhr.responseText;
+        }
+    };
+    footerXhr.send();
 });
+
+
+
--- view/js/sesion.js
+++ view/js/sesion.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const headerContainer = document.getElementById('header-container');
-    const headerHtmlFile = 'header.html'; // Ruta corregida
+    const headerHtmlFile = 'header.html'; // Ruta 
     const xhr = new XMLHttpRequest(); // Crear una nueva instancia de XMLHttpRequest
     xhr.open('GET', headerHtmlFile, true);
     xhr.onreadystatechange = function() {
        if (xhr.readyState === 4 && xhr.status === 200) {
           headerContainer.innerHTML = xhr.responseText;
+       }
+    };
+    xhr.send();
+ });
+
+
+ document.addEventListener('DOMContentLoaded', function() {
+    const footerContainer = document.getElementById('footer-container');
+    const footerHtmlFile = 'footer.html'; // Ruta 
+    const xhr = new XMLHttpRequest(); // Crear una nueva instancia de XMLHttpRequest
+    xhr.open('GET', footerHtmlFile, true);
+    xhr.onreadystatechange = function() {
+       if (xhr.readyState === 4 && xhr.status === 200) {
+          footerContainer.innerHTML = xhr.responseText;
        }
     };
     xhr.send();
