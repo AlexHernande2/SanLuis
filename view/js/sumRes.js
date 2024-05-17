041858ef524289afc31cbe6582cafb104d5971ff
Santiago <lspardo@uniboyaca.edu.co>
2024-05-17 09:27:43 -0500

    se creo la opcion de sumar o quitar unidades y el boton para agregar al carrito (aun sin funcionalidad)

---
+++ view/js/sumRes.js
@@ -0,0 +1,21 @@
+function incrementCounter(index){
+    let cont = document.getElementsByClassName('contador');
+    let maxIndex = cont[index].getAttribute('max');
+    let contText = cont[index].innerText;
+    let contInt = parseInt(contText);
+    let newValue = contInt + 1;
+    if (newValue <= maxIndex) {
+        cont[index].innerText = newValue;
+    }
+
+}
+
+function decrementCounter(index){
+    let cont = document.getElementsByClassName('contador');
+    let contText = cont[index].innerText;
+    let contInt = parseInt(contText);
+    let newValue = contInt - 1;
+    if (newValue >= 1) {
+        cont[index].innerText = newValue;
+    }
+}
