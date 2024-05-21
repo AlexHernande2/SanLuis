801d9e9a5e516bcb5f3ebb7a1df84940ea2e0f54
Sala D6 <CSalaD6@uniboyaca.edu.co>
2024-05-21 15:52:02 -0500

    Se modificaron algunas parte que ya no se usan

--- view/php/formSesion.php
+++ view/php/formSesion.php
@@ -47,9 +47,11 @@ if (empty($documento)) {
   <title>Document</title>
   <!-- Título de la página -->
   <link rel="stylesheet" href="../css/sesion.css">
+  <link rel="stylesheet" href="../css/formSesion.css">
   <!-- Enlace a la hoja de estilo CSS personalizada -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  
   <!-- Enlace a la hoja de estilo de Bootstrap -->
 </head>
 
