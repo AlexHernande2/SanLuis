71d6e3e7fae20e282d5ce3657b08fcf8b029b2e6
Sala D6 <CSalaD6@uniboyaca.edu.co>
2024-05-21 15:05:30 -0500

    Se agrego el apartado donde iran los producton en la interfaz de pedido

--- view/php/pedido.php
+++ view/php/pedido.php
@@ -17,7 +17,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pedido</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/pedido.css">
 </head>
 
@@ -27,75 +28,98 @@
     </header>
     <h1>Historial de pedido </h1>
 
-<main>
-    <!--  -->
-<table class="table table-dark table-striped">
-    
-<thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Fecha</th>
-      <th scope="col">Estado</th>
-      <th scope="col">Productos</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-        <!-- Id del pedido -->
-      <th scope="row"></th>
-      <!-- Va el contenido, Nombre, Fecha, Estado y Productos-->
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-   
-  </tbody>
+    <main>
+        <!--  -->
+        <table class="table table-dark table-striped">
 
-</table>
-<!--  -->
-<main>
-  <div class="container" id="formularioDiv">
-      <!-- Contenedor Bootstrap con ID "f" -->
-     
-     
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Fecha</th>
+                    <th scope="col">Estado</th>
+                    <th scope="col">Productos</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <!-- Id del pedido -->
+                    <th scope="row">1</th>
+                    <!-- Va el contenido, Nombre, Fecha, Estado y Productos-->
+                    <td>Cesar Martinez Avella </td>
+                    <td>21/05/2024</td>
+                    <td>Recibido</td>
+                    <td>
+
+                        <div class="container-sm"  >
+                            <div class="accordion  accordion-flush" id="accordionFlushExample" >
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                        <button class="accordion-button collapsed" type="button"
+                                            data-bs-toggle="collapse" data-bs-target="#flush-collapseOne"
+                                            aria-expanded="false" aria-controls="flush-collapseOne">
+                                            Accordion Item #1
+                                        </button>
+                                    </h2>
+                                    <div id="flush-collapseOne" class="accordion-collapse collapse"
+                                        data-bs-parent="#accordionFlushExample">
+                                        <div class="accordion-body">Placeholder content for this accordion, which is
+                                            intended to demonstrate the <code>.accordion-flush</code> </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </td>
+                </tr>
+
+            </tbody>
+
+        </table>
+
+        <!--  -->
+        <main>
+            <div class="container" id="formularioDiv">
+                <!-- Contenedor Bootstrap con ID "f" -->
 
 
 
-</main>
+
+
+        </main>
 
 
 
-    <footer>
-        <div id="footer-container"></div>
-    </footer>
-    <script src="../js/initHF.js"></script>
-    <script>
-            const $dropdown = $(".dropdown");
-            const $dropdownToggle = $(".dropdown-toggle");
-            const $dropdownMenu = $(".dropdown-menu");
-            const showClass = "show";
-            $(window).on("load resize", function() {
-                if (this.matchMedia("(min-width: 768px)").matches) {
-                    $dropdown.hover(
-                        function() {
-                            const $this = $(this);
-                            $this.addClass(showClass);
-                            $this.find($dropdownToggle).attr("aria-expanded", "true");
-                            $this.find($dropdownMenu).addClass(showClass);
-                        },
-                        function() {
-                            const $this = $(this);
-                            $this.removeClass(showClass);
-                            $this.find($dropdownToggle).attr("aria-expanded", "false");
-                            $this.find($dropdownMenu).removeClass(showClass);
-                        }
-                    );
-                } else {
-                    $dropdown.off("mouseenter mouseleave");
-                }
-            });
+        <footer>
+            <div id="footer-container"></div>
+        </footer>
+        <script src="../js/initHF.js"></script>
+        <script>
+        const $dropdown = $(".dropdown");
+        const $dropdownToggle = $(".dropdown-toggle");
+        const $dropdownMenu = $(".dropdown-menu");
+        const showClass = "show";
+        $(window).on("load resize", function() {
+            if (this.matchMedia("(min-width: 768px)").matches) {
+                $dropdown.hover(
+                    function() {
+                        const $this = $(this);
+                        $this.addClass(showClass);
+                        $this.find($dropdownToggle).attr("aria-expanded", "true");
+                        $this.find($dropdownMenu).addClass(showClass);
+                    },
+                    function() {
+                        const $this = $(this);
+                        $this.removeClass(showClass);
+                        $this.find($dropdownToggle).attr("aria-expanded", "false");
+                        $this.find($dropdownMenu).removeClass(showClass);
+                    }
+                );
+            } else {
+                $dropdown.off("mouseenter mouseleave");
+            }
+        });
         </script>
 </body>
 
