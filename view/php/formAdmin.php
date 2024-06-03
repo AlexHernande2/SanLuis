845a4ebfc180873697d406d41bc7013cf28857f1
Santiago <lspardo@uniboyaca.edu.co>
2024-06-02 21:17:51 -0500

    se solucion el error de animacion en los input

--- view/php/formAdmin.php
+++ view/php/formAdmin.php
@@ -25,6 +25,7 @@ if($admin){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/styless.css">
     <link rel="stylesheet" href="../css/formAdmin.css">
@@ -134,12 +135,12 @@ if($admin){
 
 
     
-    <script src="../js/jquery-3.3.1.min.js"></script>
+    <!-- <script src="../js/jquery-3.3.1.min.js"></script> -->
     <script src="../js/busqueda.js"></script>
     <script src="../js/header.js"></script>
     <script src="../js/formAdmin.js"></script>
     <script src="../js/initHF.js"></script>
-
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
  
